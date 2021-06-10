dcbac2211dd7cda50d813da43645cd5449fb58a4
alesamv <alesamv@gmail.com>
2021-06-09 20:03:46 -0500

    .

--- js/app.js
+++ js/app.js
@@ -5,57 +5,32 @@ const url = 'https://newsapi.org/v2/everything?' +
 'apiKey=d56e63af081f44c9ac43b3bc5a59619a';
 
 //Get the data and display in the html
-const data = (newsdata) => {
-    const articlesDiv = document.querySelector(".news")
-    newsdata.articles.forEach((article) => {
+const infoNewCard = (info) => {
+    const newsCardContainer = document.querySelector(".news")
+    info.articles.forEach((infoNewCard) => {
 
-	    //Agregadno una noticia
+
+	    //Adding a new card
         const div = document.createElement("div")
         div.className = "new"
         div.innerHTML = `
     
-            <img src="${article.urlToImage}" alt='${article.title}'/>
+            <img src="${infoNewCard.urlToImage}" alt='${infoNewCard.title}'/>
             <div class="data">
-                <h3>${article.source.name}</h3>
-                <h4>${article.publishedAt}</h4>
+                <h3>${infoNewCard.source.name}</h3>
+                <h4>${infoNewCard.publishedAt}</h4>
             </div>
-            <h2>${article.title}</h2>
-            <p>${article.description}</p>
+            <h2>${infoNewCard.title}</h2>
+            <p>${infoNewCard.description}</p>
             <div class ='btn'>
-                <a class='btn-new' target='_blank' href="${article.url}">Read more</a>
+                <a class='btn-new' target='_blank' href="${infoNewCard.url}">Read more</a>
             </div>
             `
-      articlesDiv.appendChild(div)
+      newsCardContainer.appendChild(div)
 			
     })
 }
 
-
-
 fetch(url)
     .then(response => response.json())
-    .then(data)
-
-
-
-
-
-
-
-
-
-
-
-//Fetch sends a request to the API.
-//Promise makes it possible to run this in the background. När vi får APIet då går den vidare och skickar tillbaka JSON.
-
-
-
-
-
-var req = new Request(url);
-
-fetch(req)
-    .then(function(response) {
-        console.log(response.json());
-    })
+    .then(infoNewCard)
